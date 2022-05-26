906f3cde76aa1d55e2be989218ad86e12ecdb55e
YigitCanBUDAK <budakyigitcan26@gmail.com>
2022-05-27 01:37:42 +0300

    E Ticaret

---
+++ ETicaretClient/src/app/admin/components/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import{ AlertifyService, MessageType, Position} from 'src/app/services/admin/alertify.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent extends BaseComponent implements OnInit {
+
+  constructor(private alerttify: AlertifyService,spinner:NgxSpinnerService) {
+    super(spinner);
+   }
+
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallAtom);
+  }
+  m(){
+    this.alerttify.message("Merhaba",{
+      messageType: MessageType.Success,
+      delay:5,
+      position:Position.TopRight
+
+
+    })
+
+  }
+  d(){
+    this.alerttify.dismiss()
+  }
+
+}
